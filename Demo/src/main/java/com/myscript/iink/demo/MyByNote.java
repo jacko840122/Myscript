c4b8b3135faf919b6db88cdd976db0808e59e006
lsr <305196431@qq.com>
2019-01-19 20:37:39 +0800

    add 4566

---
+++ Demo/src/main/java/com/myscript/iink/demo/MyByNote.java
@@ -0,0 +1,16 @@
+package com.myscript.iink.demo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.by.hw.drawcomponent.ByNote;
+
+public class MyByNote extends ByNote {
+    public MyByNote(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+    }
+
+    public MyByNote(Context context) {
+        super(context);
+    }
+}
---
+++ UIReferenceImplementation/src/main/java/com/note/book/bean/PointEvent.java
@@ -0,0 +1,74 @@
+package com.note.book.bean;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Created by fiona
+ * Time 2019/1/10 22:18
+ * Describe:点阵
+ */
+public class PointEvent implements Serializable, Cloneable {
+    static final long serialVersionUID =7339186976632615630L;
+    public int eventType; //DOWN=0,MOVE,UP,CANCEL;
+    public float x;
+    public float y;
+    public int pointType; //PEN=0,TOUCH,ERASER;
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        // TODO Auto-generated method stub
+        PointEvent pointEvent = null;
+        try {
+            pointEvent = (PointEvent) super.clone();
+        } catch (CloneNotSupportedException e) {
+        }
+        return pointEvent;
+    }
+
+    /**
+     * 序列化
+     * @param file 文件
+     * @param arrays 对象列表
+     * @throws IOException
+     */
+    public static void serialize(File file, List<PointEvent> arrays) throws IOException {
+        if (file == null || arrays == null) {
+            return;
+        }
+
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+
+        OutputStream os = new FileOutputStream(file);
+        ObjectOutputStream oos = new ObjectOutputStream(os);
+        oos.writeObject(arrays);
+        oos.close();
+    }
+
+    /**
+     *
+     * @param file
+     * @return
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    public static List<PointEvent> deserialize(File file) throws IOException,
+            ClassNotFoundException {
+        InputStream is = new FileInputStream(file);
+        ObjectInputStream ois = new ObjectInputStream(is);
+        List<PointEvent> obj = (ArrayList<PointEvent>)ois.readObject();
+        ois.close();
+        return obj;
+    }
+}
